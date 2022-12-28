40a3089f3179da6b0f5995743362ab4229a8d5fc
miyoun116 <miyoun116@gmail.com>
2022-12-28 11:47:25 -0800

    initial web page

--- wnm601/hw7/cardflip_cho.js
+++
@@ -1,6 +0,0 @@
-function flip (event)
-{
-	var element = event.currentTarget;
-	/* Toggle the setting of the classname attribute */
-	element.className = (element.className == 'card') ? 'card flipped' : 'card';
-}
